7238804a0c51b07e9db99ca1bea8bc25e0faf9fd
cr4kn4x <s.heck0807@gmail.com>
2021-06-27 15:10:04 +0200

    shopDaoTest

--- Backend/Tests/shopDao.test.js
+++ Backend/Tests/shopDao.test.js
@@ -6,13 +6,35 @@ const dbConnection = new Database(dbFile, dbOptions);
 
 
 
-
-//BETA
 const ShopDao  = require('../dao/shopDao');
 const { TestWatcher } = require("jest");
 const shopDao = new ShopDao(dbConnection);
 
-test('ShopDAO alle Bücher' , () => {
-  expect(shopDao.loadSuche("HFD(")).toStrictEqual([]);
+
+
+test('ShopDao alle Bücher laden' , () => {
+    let result = shopDao.loadAll();
+    expect(result).toBeDefined();
+    expect(CheckBuchAttribute(result)).toBeTruthy();
 })
-//BETA
+
+
+function CheckBuchAttribute(result) {
+    if(result.length==0){return true} // Es sind keine Bücher in der Datenbank --> True
+    for(let i=0; i<result.length;i++){
+        if(result[i].anzahlbew==undefined){throw new Error("anzahlbew nicht definiert!")}
+        if(result[i].authorid==undefined){throw new Error("authorid nicht definiert!")}
+        if(result[i].autor_name==undefined){throw new Error("autor_name nicht definiert!")}
+        if(result[i].bildid==undefined){throw new Error("bildid nicht definiert!")}
+        if(result[i].bildpfad==undefined){throw new Error("bildpfad nicht definiert!")}
+        if(result[i].genreid==undefined){throw new Error("genreid nicht definiert!")}
+        if(result[i].gesamtbewertung==undefined){throw new Error("gesamtbewertung nicht definiert!")}
+        if(result[i].id==undefined){throw new Error("id nicht definiert!")}
+        if(result[i].isbn==undefined){throw new Error("isbn nicht definiert!")}
+        if(result[i].kurzbeschreibung==undefined){throw new Error("kurzbeschreibung nicht definiert!")}
+        if(result[i].mehrwertsteuerid==undefined){throw new Error("mehrwertsteuerid nicht definiert!")}
+        if(result[i].preis==undefined){throw new Error("preis nicht definiert!")}
+        if(result[i].titel==undefined){throw new Error("titel nicht definiert!")}
+    }
+    return true;
+}
